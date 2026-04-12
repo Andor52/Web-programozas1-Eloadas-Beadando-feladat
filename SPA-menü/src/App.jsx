c399a1df53d9a2bc0872d24f95c4159019cd78a7
MAndor_AKYY5Z <makaiandor52@gmail.com>
2026-04-12 10:27:22 +0200

    SPA-menu javítások
    Két mappa került feltöltésre, egyikben az egész SPA menü teljes forrása van. A másikban pedig csak az src és a dist fájlok vannak.

---
+++ SPA-menü/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import ToDoApp from './todo/todoapp';
+import Bill from './bill/AApp';
+import './App.css'
+
+
+function App() {
+  const [page, setPage] = useState(true);
+
+  return (
+    <>
+    <header>
+        <h1>React SPA megvalósítás</h1>
+    </header>
+    <main>
+    <button onClick={( )=>{setPage(true);}} class="todo">ToDo</button>
+    <button onClick={( )=>{setPage(false);}} class="bill">Bill</button>
+    {
+      page && <ToDoApp/>
+    }
+    {
+      !page && <Bill/>
+    }
+    </main>
+    <footer>Készítők: Nyikos Diána - CU4KRP || Makai Andor - AKYY5Z</footer>
+    </>
+  )
+}
+
+export default App
